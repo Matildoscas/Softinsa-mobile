36353f92ece21cdeeaf59b85e3e98fc3b79930e3
matildoscas <matidossantos55@gmail.com>
2026-04-22 10:29:50 +0100

    certificado consultor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart'; // Importa a biblioteca principal do Flutter
-import 'login.dart'; 
+//import 'login.dart'; 
+import 'certificado.dart';
+//import 'database_service.dart';
 
 void main() {
   runApp(MyApp()); // Inicializa a app
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Remove o banner de debug
-      home: LoginPage(), // Define a página de login
+      home: CertificadoPage(userData: {}), // Define a página de login
     );
   }
 }
