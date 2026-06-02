238f9085336badf66d07f98fdf04895c1ac9330b
matildoscas <matidossantos55@gmail.com>
2026-06-02 09:52:57 +0100

    Firebase implementação

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,16 @@ import 'dart:convert';
 import 'screens/login.dart';
 import 'screens/pagina_principal.dart';
 import 'screens/Perfil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
